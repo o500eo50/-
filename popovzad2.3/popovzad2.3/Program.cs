e80126445621dc9f6facbfee4ab6938a7092142d
o500eo50 <popov.va.06@mail.ru>
2025-01-13 01:11:35 +0300

    Add files via upload

---
+++ popovzad2.3/popovzad2.3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Объявление переменных
+        string str1 = "Бедность";
+        string str2 = " – не порок, а большое несчастье.";
+
+        // Конкатенация строк
+        Console.WriteLine(str1 + str2);
+    }
+}
