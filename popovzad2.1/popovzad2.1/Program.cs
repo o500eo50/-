fcdb0a1ae7b8c8437e6d159275eef34c2358f6d9
o500eo50 <popov.va.06@mail.ru>
2025-01-13 01:09:22 +0300

    Add files via upload

---
+++ popovzad2.1/popovzad2.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Объявление и инициализация констант
+        int I = 1;                   
+        string II = "хорошо";        
+        char III = 'a';              
+        int IV = 2;                  
+
+        // Форматированный вывод сообщения
+        Console.WriteLine("{0} голова {1}, {2} {3} – лучше", I, II, III, IV);
+    }
+}
