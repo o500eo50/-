e3f035d12217cc0f8f8712ec7affa2d130584b98
o500eo50 <popov.va.06@mail.ru>
2025-01-13 01:13:28 +0300

    Add files via upload

---
+++ popovzad2.4/popovzad2.4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Исходная строка с ошибками
+        string text = "Веръшины, пятынистые, сеедой, порялдели, очищають, трясеёт, приппекает, помешарть";
+
+        // Замена опечаток с использованием Remove() и исправление текста
+        text = text.Replace("ъ", "").Replace("еед", "ед").Replace("ря", "ре").Replace("яь", "я").Replace("её", "ет");
+        text = text.Replace("пп", "п").Replace("шарть", "шать");
+
+        // Вывод исправленной строки
+        Console.WriteLine(text);
+    }
+}
